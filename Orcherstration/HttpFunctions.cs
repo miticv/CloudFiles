4bb53b2a427ee5b9313ba27cbeb32452a18941fa
Vladimir Mitic <miticv@gmail.com>
2021-09-01 21:24:30 -0400

    Api to list processes

--- Orcherstration/HttpFunctions.cs
+++ Orcherstration/HttpFunctions.cs
@@ -27,8 +27,8 @@ namespace CloudFiles
         [OpenApiParameter(name: "prefix", In = ParameterLocation.Query, Required = false, Type = typeof(string), Summary = "prefix of instanceId", Visibility = OpenApiVisibilityType.Advanced)]
         [OpenApiParameter(name: "showInput", In = ParameterLocation.Query, Required = false, Type = typeof(bool), Summary = "show job input", Visibility = OpenApiVisibilityType.Advanced)]
         [OpenApiParameter(name: "pageSize", In = ParameterLocation.Query, Required = false, Type = typeof(int), Summary = "records per page", Visibility = OpenApiVisibilityType.Advanced)]
-        [OpenApiParameter(name: "from", In = ParameterLocation.Query, Required = false, Type = typeof(DateTime), Summary = "filter from creation date", Visibility = OpenApiVisibilityType.Advanced)]
-        [OpenApiParameter(name: "to", In = ParameterLocation.Query, Required = false, Type = typeof(DateTime), Summary = "filter to creation date", Visibility = OpenApiVisibilityType.Advanced)]
+        [OpenApiParameter(name: "from", In = ParameterLocation.Query, Required = false, Type = typeof(DateTime), Summary = "filter from creation date", Visibility = OpenApiVisibilityType.Advanced, Description = "Format: MM/dd/yyyy HH:mm:ss")]
+        [OpenApiParameter(name: "to", In = ParameterLocation.Query, Required = false, Type = typeof(DateTime), Summary = "filter to creation date", Visibility = OpenApiVisibilityType.Advanced, Description = "Format: MM/dd/yyyy HH:mm:ss")]
         [OpenApiParameter(name: "statusList", In = ParameterLocation.Query, Required = false, Type = typeof(List<OrchestrationRuntimeStatus>), Summary = "List of statuses to show", Visibility = OpenApiVisibilityType.Advanced,
             Description = "0:Running, 1:Completed, 2:New: 3:Failed, 4:Canceled, 5:Terminated, 6:Pending")]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(OrchestrationStatusQueryResult),
@@ -41,70 +41,78 @@ namespace CloudFiles
            [DurableClient] IDurableOrchestrationClient starter,
            ILogger log)
         {
-            _ = await GoogleUtility.VerifyGoogleHeaderTokenIsValid(req).ConfigureAwait(false);
-            var queryFilter = new OrchestrationStatusQueryCondition();
-            var statuses = new List<OrchestrationRuntimeStatus>();
+            try {
+                _ = await GoogleUtility.VerifyGoogleHeaderTokenIsValid(req).ConfigureAwait(false);
+                var queryFilter = new OrchestrationStatusQueryCondition();
+                var statuses = new List<OrchestrationRuntimeStatus>();
 
-            int pageSizeInt = 500;
-            var intSuccess = string.IsNullOrEmpty(req.Query["pageSize"]) || int.TryParse(req.Query["pageSize"], out pageSizeInt);
-            if (!intSuccess)
-            {
-                throw new InvalidOperationException("pageSize must be int.");
-            }
-
-            var showInput = true;
-            var boolSuccess = string.IsNullOrEmpty(req.Query["showInput"]) || bool.TryParse(req.Query["showInput"], out showInput);
-            if (!boolSuccess)
-            {
-                throw new InvalidOperationException("showInput must be boolean.");
-            }
-
-            var fromDate = DateTime.MinValue;
-            var dateSuccess = string.IsNullOrEmpty(req.Query["from"]) || DateTime.TryParse(req.Query["from"], out fromDate);
-            if (!dateSuccess)
-            {
-                throw new InvalidOperationException("from must be date.");
-            }
-
-            var toDate = DateTime.MaxValue;
-            dateSuccess = string.IsNullOrEmpty(req.Query["to"]) || DateTime.TryParse(req.Query["to"], out toDate);
-            if (!dateSuccess)
-            {
-                throw new InvalidOperationException("to must be date.");
-            }
-
-            //  Running = 0        The orchestration is running (it may be actively running or waiting for input).
-            //  Completed = 1      The orchestration ran to completion.
-            //  ContinuedAsNew = 2 The orchestration completed with ContinueAsNew as is in the process of restarting.
-            //  Failed = 3         The orchestration failed with an error.
-            //  Canceled = 4       The orchestration was canceled.
-            //  Terminated = 5     The orchestration was terminated via an API call.
-            //  Pending = 6        The orchestration was scheduled but has not yet started.
-            const string statusListAll = "0,1,2,3,4,5,6";
-            string[] statusList = string.IsNullOrEmpty(req.Query["statusList"]) ? statusListAll.Split(",") : (req.Query["statusList"].ToString()).Split(",");
-            foreach (var i in statusList) {
-                var success = int.TryParse(i, out int status);
-                if (success)
+                int pageSizeInt = 500;
+                var intSuccess = string.IsNullOrEmpty(req.Query["pageSize"]) || int.TryParse(req.Query["pageSize"], out pageSizeInt);
+                if (!intSuccess)
                 {
-                    statuses.Add((OrchestrationRuntimeStatus)status);
+                    throw new InvalidOperationException("pageSize must be int.");
                 }
-                else {
-                    throw new InvalidOperationException($"Item in statusList: `{i}` is not an integer.");
+
+                var showInput = true;
+                var boolSuccess = string.IsNullOrEmpty(req.Query["showInput"]) || bool.TryParse(req.Query["showInput"], out showInput);
+                if (!boolSuccess)
+                {
+                    throw new InvalidOperationException("showInput must be boolean.");
                 }
+
+                var fromDate = DateTime.MinValue;
+                var dateSuccess = string.IsNullOrEmpty(req.Query["from"]) || DateTime.TryParse(req.Query["from"], out fromDate);
+                if (!dateSuccess)
+                {
+                    throw new InvalidOperationException("from must be date.");
+                }
+
+                var toDate = DateTime.MaxValue;
+                dateSuccess = string.IsNullOrEmpty(req.Query["to"]) || DateTime.TryParse(req.Query["to"], out toDate);
+                if (!dateSuccess)
+                {
+                    throw new InvalidOperationException("to must be date.");
+                }
+
+                //  Running = 0        The orchestration is running (it may be actively running or waiting for input).
+                //  Completed = 1      The orchestration ran to completion.
+                //  ContinuedAsNew = 2 The orchestration completed with ContinueAsNew as is in the process of restarting.
+                //  Failed = 3         The orchestration failed with an error.
+                //  Canceled = 4       The orchestration was canceled.
+                //  Terminated = 5     The orchestration was terminated via an API call.
+                //  Pending = 6        The orchestration was scheduled but has not yet started.
+                const string statusListAll = "0,1,2,3,4,5,6";
+                string[] statusList = string.IsNullOrEmpty(req.Query["statusList"]) ? statusListAll.Split(",") : (req.Query["statusList"].ToString()).Split(",");
+                foreach (var i in statusList)
+                {
+                    var success = int.TryParse(i, out int status);
+                    if (success)
+                    {
+                        statuses.Add((OrchestrationRuntimeStatus)status);
+                    }
+                    else
+                    {
+                        throw new InvalidOperationException($"Item in statusList: `{i}` is not an integer.");
+                    }
+                }
+
+                queryFilter.CreatedTimeFrom = fromDate;
+                queryFilter.CreatedTimeTo = toDate;
+                queryFilter.TaskHubNames = new[] { starter.TaskHubName };
+                queryFilter.ContinuationToken = req.Query["continueToken"];
+                queryFilter.InstanceIdPrefix = req.Query["prefix"];
+                queryFilter.ShowInput = showInput;
+                queryFilter.PageSize = pageSizeInt;
+                queryFilter.RuntimeStatus = statuses;
+
+                log.LogInformation($"List orchestration instances.");
+                var result = await starter.ListInstancesAsync(queryFilter, CancellationToken.None).ConfigureAwait(false);
+                return new OkObjectResult(result);
+
+            } catch (UnauthorizedAccessException ex) {
+                log.LogError(ex.Message);
+                return new StatusCodeResult(StatusCodes.Status401Unauthorized);
             }
-
-            queryFilter.CreatedTimeFrom = fromDate;
-            queryFilter.CreatedTimeTo = toDate;
-            queryFilter.TaskHubNames = new[] { starter.TaskHubName };
-            queryFilter.ContinuationToken = req.Query["continueToken"];
-            queryFilter.InstanceIdPrefix = req.Query["prefix"];
-            queryFilter.ShowInput = showInput;
-            queryFilter.PageSize = pageSizeInt;
-            queryFilter.RuntimeStatus = statuses;
-
-            log.LogInformation($"List orchestration instances.");
-            var result = await starter.ListInstancesAsync(queryFilter, CancellationToken.None).ConfigureAwait(false);
-            return new OkObjectResult(result);
         }
 
         [OpenApiOperation(operationId: Constants.ProcessAzureToGooglePhotos_Start, tags: new[] { "Processes" }, Summary = "Start Azure to Google Photos job", Description = "Single tenant for now.", Visibility = OpenApiVisibilityType.Important)]
@@ -120,20 +128,25 @@ namespace CloudFiles
             [DurableClient] IDurableOrchestrationClient starter,
             ILogger log)
         {
-            var accessToken = await GoogleUtility.VerifyGoogleHeaderTokenIsValid(req).ConfigureAwait(false);
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync().ConfigureAwait(false);
-            var request = JsonConvert.DeserializeObject<FilesCopyRequest>(requestBody);
+            try {
+                var accessToken = await GoogleUtility.VerifyGoogleHeaderTokenIsValid(req).ConfigureAwait(false);
+                string requestBody = await new StreamReader(req.Body).ReadToEndAsync().ConfigureAwait(false);
+                var request = JsonConvert.DeserializeObject<FilesCopyRequest>(requestBody);
 
-            if (request.SelectedItemsList == null || request.SelectedItemsList.Count == 0)
-            {
-                return new BadRequestObjectResult($"{nameof(request.SelectedItemsList)} must contain items");
+                if (request.SelectedItemsList == null || request.SelectedItemsList.Count == 0)
+                {
+                    return new BadRequestObjectResult($"{nameof(request.SelectedItemsList)} must contain items");
+                }
+                request.AccessToken = accessToken;
+
+                string instanceId = await starter.StartNewAsync(Constants.AzureStorageToGooglePhotosOrchestrator, null, request).ConfigureAwait(false);
+                log.LogInformation($"Started orchestration instance '{instanceId}'.");
+
+                return starter.CreateCheckStatusResponse(req, instanceId);
+            } catch (UnauthorizedAccessException ex) {
+                    log.LogError(ex.Message);
+                    return new StatusCodeResult(StatusCodes.Status401Unauthorized);
             }
-            request.AccessToken = accessToken;
-
-            string instanceId = await starter.StartNewAsync(Constants.AzureStorageToGooglePhotosOrchestrator, null, request).ConfigureAwait(false);
-            log.LogInformation($"Started orchestration instance '{instanceId}'.");
-
-            return starter.CreateCheckStatusResponse(req, instanceId);
         }
 
         [OpenApiOperation(operationId: Constants.ProcessGooleStorageToGooglePhotos_Start, tags: new[] { "Processes" }, Summary = "Start Google Storage to Google Photos job", Description = "Single tenant for now", Visibility = OpenApiVisibilityType.Important)]
@@ -150,20 +163,26 @@ namespace CloudFiles
             [DurableClient] IDurableOrchestrationClient starter,
             ILogger log)
         {
-            var accessToken = await GoogleUtility.VerifyGoogleHeaderTokenIsValid(req).ConfigureAwait(false);
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync().ConfigureAwait(false);
-            var request = JsonConvert.DeserializeObject<FilesCopyRequest>(requestBody);
+            try {
+                var accessToken = await GoogleUtility.VerifyGoogleHeaderTokenIsValid(req).ConfigureAwait(false);
+                string requestBody = await new StreamReader(req.Body).ReadToEndAsync().ConfigureAwait(false);
+                var request = JsonConvert.DeserializeObject<FilesCopyRequest>(requestBody);
 
-            if (request.SelectedItemsList == null || request.SelectedItemsList.Count == 0)
-            {
-                return new BadRequestObjectResult($"{nameof(request.SelectedItemsList)} must contain items");
+                if (request.SelectedItemsList == null || request.SelectedItemsList.Count == 0)
+                {
+                    return new BadRequestObjectResult($"{nameof(request.SelectedItemsList)} must contain items");
+                }
+                request.AccessToken = accessToken;
+
+                string instanceId = await starter.StartNewAsync(Constants.GooleStorageToGooglePhotosOrchestrator, null, request).ConfigureAwait(false);
+                log.LogInformation($"Started orchestration instance '{instanceId}'.");
+
+                return starter.CreateCheckStatusResponse(req, instanceId);
+
+            } catch (UnauthorizedAccessException ex) {
+                log.LogError(ex.Message);
+                return new StatusCodeResult(StatusCodes.Status401Unauthorized);
             }
-            request.AccessToken = accessToken;
-
-            string instanceId = await starter.StartNewAsync(Constants.GooleStorageToGooglePhotosOrchestrator, null, request).ConfigureAwait(false);
-            log.LogInformation($"Started orchestration instance '{instanceId}'.");
-
-            return starter.CreateCheckStatusResponse(req, instanceId);
         }
     }
 }
